a385745bbbc794b4311212e9b0ae4cb040f5b77b
Pommy <pomboivo@hotmail.com>
2021-10-14 15:57:00 +0700

    Update Design

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from "react";
-import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, ImageBackground } from "react-native";
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import Header from "../components/Header";
 
 const LoginScreen = (props) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Police Report</Text>
+      <Header title="Police Report" />
+      <br/>
       <TextInput
         style={styles.input}
         placeholder="Email"
@@ -20,11 +22,15 @@ const LoginScreen = (props) => {
       />
       <br/>
       <TouchableOpacity style={styles.buttonLogin}>
-        <Text>Login</Text>
+        <Text style={styles.fontButton}>Log in</Text>
       </TouchableOpacity>
+      <br/>
       <TouchableOpacity style={styles.buttonRegister}>
-        <Text>Register</Text>
+        <Text style={styles.fontButton}>Sign Up</Text>
       </TouchableOpacity>
+      <br/>
+      <Text style={styles.font}><i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;forgot your password?</i></Text>
+      <br/>
     </SafeAreaView>
   );
 };
@@ -35,9 +41,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  fontButton: {
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  font: {
+    color: 'white',
+    alignSelf: 'flex-start'
+  },
   input: {
     height: 40,
-    width: '100%',
+    width: '80%',
     borderWidth: 1,
     borderBottomColor: 'white',
     padding: 10,
@@ -53,14 +67,14 @@ const styles = StyleSheet.create({
     paddingBottom: 70,
   },
   buttonLogin: {
-    width: '100%',
+    width: '80%',
     height: 40,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#60b45c",
   },
   buttonRegister: {
-    width: '100%',
+    width: '80%',
     height: 40,
     justifyContent: "center",
     alignItems: "center",
